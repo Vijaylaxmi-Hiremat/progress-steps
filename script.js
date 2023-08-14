ac884ec4bd45b43cb98299132de22274e669608f
Vijaylaxmi-Hiremat <vijaylaxmis010@gmail.com>
2023-08-14 18:28:13 +0530

    added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-const circles = document.getElementById('circle')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
 next.addEventListener('click', () => {
     currentActive++
 
-    if(currentActive > circles.lenght){
-        currentActive = circles.lenght
+    if(currentActive > circles.length) {
+        currentActive = circles.length
     }
 
     update()
@@ -18,32 +18,32 @@ next.addEventListener('click', () => {
 prev.addEventListener('click', () => {
     currentActive--
 
-    if(currentActive < 1){
+    if(currentActive < 1) {
         currentActive = 1
     }
 
     update()
 })
 
-function update(){
+function update() {
     circles.forEach((circle, idx) => {
-        if(idx < currentActive){
+        if(idx < currentActive) {
             circle.classList.add('active')
-        } else{
+        } else {
             circle.classList.remove('active')
         }
     })
 
-const actives = document.querySelectorAll('.active')
+    const actives = document.querySelectorAll('.active')
 
-progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-if(currentActive === 1){
-    prev.disabled =true
-} else if(currentActive === circles.lenght){
-    next.disabled = true
-} else {
-    prev.disabled = false
-    next.disabled = false
-}
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
